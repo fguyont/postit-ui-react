95f8a44c5b9f524228a84301bdc19801bf703371
Frederic Guyont <you@example.com>
2026-01-12 01:31:47 +0100

    ADD models for login request and success

---
+++ src/models/LoginRequest.ts
@@ -0,0 +1,4 @@
+export type LoginSuccess = {
+    email: string;
+    password: string;
+}
---
+++ src/models/LoginSuccess.ts
@@ -0,0 +1,5 @@
+export type LoginSuccess = {
+    name: string;
+    email: string;
+    token: string;
+}
