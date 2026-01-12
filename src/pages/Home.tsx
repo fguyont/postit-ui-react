5af66b5872063dab69139880cabc0b705b43548f
Frederic Guyont <you@example.com>
2026-01-12 01:34:32 +0100

    ADD homepage

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+function Home() {
+  return (
+    <div>
+      <h1>This is the home page</h1>
+      <Link to="login"><button>Login</button></Link>
+      <Link to="register"><button>Register</button></Link>
+    </div>
+  );
+}
+
+export default Home;
